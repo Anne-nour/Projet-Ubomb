5d11d12d7065f0bab5b2ba0dadc4b7797fdf64bb
anne-nour <anne.nour.marbou@gmail.com>
2021-01-05 19:00:58 +0200

    Modif  player

--- UBomb-student/src/main/java/fr/ubx/poo/model/go/character/Player.java
+++ UBomb-student/src/main/java/fr/ubx/poo/model/go/character/Player.java
@@ -60,6 +60,7 @@ public class Player extends GameObject implements Movable {
 
         Position nextPos = direction.nextPosition(getPosition());
         Decor decor = game.getWorld().get(nextPos);
+        Decor decor_next = game.getWorld().get(direction.nextPosition(nextPos));
 
         if ( !nextPos.inside( this.game.getWorld().dimension ) ){
             return false;
@@ -84,8 +85,11 @@ public class Player extends GameObject implements Movable {
         
         if (decor instanceof Box){
             System.out.println(game.getWorld());
+            if ( decor_next instanceof Stone ||  decor_next instanceof Tree ||  decor_next instanceof Box
+                    ||  decor_next instanceof Monster ||  ! direction.nextPosition(nextPos).inside( this.game.getWorld().dimension )  ) {
+                return false;
+            }
             game.getWorld().clear(nextPos);
-
             game.getWorld().set(direction.nextPosition(nextPos), decor);
             System.out.println("===================");
             System.out.println(game.getWorld());
